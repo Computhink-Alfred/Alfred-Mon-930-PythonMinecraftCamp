350f4dce443f7c2e06cc9af311f1529d1034d4f5
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-11-18 04:35:59 +0000

    Day 1

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,30 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+
+# print("1st print statement")
+# print("4th print statement")
+# print("2nd print statement")
+# print("3rd print statement")
+
+# num1 = "10"
+# num2 = 15
+# print(num1 + str(num2))
+
+# num1 = 5
+# num2 = 10
+# print(num1 / num2)
+
+def introduceMyself(name, lastName):
+    print("My name is " + name)
+    print("My full name is " + name + " " + lastName)
+    print("My friends call me " + name)
+    print("My last name is " + lastName)
+
+# introduceMyself("Alfred", "Chan")
+# introduceMyself("Thomas", "Lim")
+
+def addition(num1, num2):
+    print(num1 + num2)
+
+addition(10, 20)
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,36 @@
-# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
-# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
-################# Function Section #############################
+def teleport():
+    agent.teleport_to_player()
 
+def moveForward():
+    agent.move(FORWARD, 1)
 
-################## On Chat Commands Section #####################
+def moveBackward():
+    agent.move(BACK, 1)
+
+def moveLeft():
+    agent.move(LEFT, 1)
+
+def moveRight():
+    agent.move(RIGHT, 1)
+
+def moveUp():
+    agent.move(UP, 1)
+
+def moveDown():
+    agent.move(DOWN, 1)
+
+def turnLeft():
+    agent.turn(LEFT)
+
+def turnRight():
+    agent.turn(RIGHT)
+
+player.on_chat("come", teleport)
+player.on_chat("fw", moveForward)
+player.on_chat("bk", moveBackward)
+player.on_chat("ml", moveLeft)
+player.on_chat("mr", moveRight)
+player.on_chat("up", moveUp)
+player.on_chat("down", moveDown)
+player.on_chat("tl", turnLeft)
+player.on_chat("tr", turnRight)
