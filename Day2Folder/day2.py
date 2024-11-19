875113f72f538ee9f9d44be990116ab1b80ef235
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-11-19 04:29:36 +0000

    Day 2

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,4 +1,12 @@
 # Write all your codes for Day 2 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day2")
+
+# for i in range(10):
+#     print(str(i+1) + ". I will not sling mud at my friends again")
+
+name = "ALFRED"
+
+print("It's")
+for i in name:
+    print(i +"!")
