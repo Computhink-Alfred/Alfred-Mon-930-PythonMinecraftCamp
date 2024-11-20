b0a7d8cb2f6ac6fea1e13da265f21df160084ffe
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-11-20 04:31:35 +0000

    Day 3

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,4 +1,76 @@
 # Write all your codes for Day 3 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# name = input("What is your name? ")
+
+# print("Welcome back, " + name + "!")
+
+# num1 = input("What is the 1st number to add? ")
+# num2 = input("What is the 2nd number to add? ")
+
+# ans = int(num1) + int(num2)
+# print("The sum of " + num1 + " and " + num2 + " = " + str(ans))
+
+
+
+
+
+# name = "Alfred"
+
+# print("My name is " + name)
+
+# input("What is your age? ") "15"
+
+# print(10 + 5) #15
+# print(int("10") + int("5")) #105
+
+# num1 = int(input("What is the 1st number? "))
+# num2 = int(input("What is the 2nd number? "))
+
+# print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
+# print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
+# print(str(num1) + " x " + str(num2) + " = " + str(num1 * num2))
+# print(str(num1) + " / " + str(num2) + " = " + str(num1 / num2))
+
+# correctPassword = "ABCD"
+# userPassword = input("What is the password? ")
+
+# if correctPassword == userPassword:
+#     print("Access Granted!")
+#     name = input("What is your name? ")
+#     print("Welcome home, " + name + "!")
+
+# if correctPassword != userPassword:
+#     print("I'm calling the police")
+
+
+
+# YiHeng = int(input("What is your age? "))
+# Jared = int(input("What is your age? "))
+
+# if YiHeng == Jared:
+#     print("Scenario 1")
+# elif YiHeng > Jared:
+#     print("Scenario 2")
+# elif YiHeng < Jared:
+#     print("Scenario 3")
+
+# if YiHeng > Jared:
+#     print("Scenario 1")
+
+# if YiHeng == Jared:
+#     print("Scenario 2")
+
+# if YiHeng < Jared:
+#     print("Scenario 3")
+
+
+import random
+
+randomNum = random.randint(1,5)
+userGuess = int(input("Guess a number between 1 and 5: "))
+
+if userGuess == randomNum:
+    print("You are correct!")
+else:
+    print("You are wrong!")
--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,63 @@
-# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
-# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def teleport():
+    agent.teleport_to_player()
+
+def moveForward(steps):
+    agent.move(FORWARD, steps)
+
+def moveBackward(steps):
+    agent.move(BACK, steps)
+
+def moveLeft(steps):
+    agent.move(LEFT, steps)
+
+def moveRight(steps):
+    agent.move(RIGHT, steps)
+
+def moveUp(steps):
+    agent.move(UP, steps)
+
+def moveDown(steps):
+    agent.move(DOWN, steps)
+
+def turnLeft():
+    agent.turn(LEFT)
+
+def turnRight():
+    agent.turn(RIGHT)
+
+def destroyTree(steps):
+    for i in range(steps):
+        agent.destroy(FORWARD)
+        agent.move(UP, 1)
+    agent.move(DOWN, steps)
+    agent.collect_all()
+
+def bulldoze(steps):
+    for i in range(steps):
+        agent.destroy(FORWARD)
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+
+def buildWall():
+    for i in range(5):
+        for i in range(3):
+            agent.place(FORWARD)
+            agent.move(UP, 1)
+        agent.move(DOWN, 3)
+        agent.move(RIGHT, 1)
+
+player.on_chat("come", teleport)
+player.on_chat("fw", moveForward)
+player.on_chat("bk", moveBackward)
+player.on_chat("ml", moveLeft)
+player.on_chat("mr", moveRight)
+player.on_chat("up", moveUp)
+player.on_chat("down", moveDown)
+player.on_chat("tl", turnLeft)
+player.on_chat("tr", turnRight)
+player.on_chat("dup", destroyTree)
+player.on_chat("mine", bulldoze)
+player.on_chat("wall", buildWall)
